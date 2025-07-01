fa5648896ee117b256dd2d20c37876874ec179d2
SuneelMadana <60563412+suneel00@users.noreply.github.com>
2025-07-01 23:25:10 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 BUILD_NUMBER=${BUILD_NUMBER}
 let PRE_BUILD=$BUILD_NUMBER-1
-sed -i "s/argo-app:$PRE_BUILD/argo-app:$BUILD_NUMBER/g" manifest/deployment.yml
+sed -i "s/ultimate-cicd:$PRE_BUILD/ultimate-cicd:$BUILD_NUMBER/g" manifest/deployment.yml
